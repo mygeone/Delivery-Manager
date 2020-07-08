857dc9d5f34c764b6aedb3c05c95f6c02a295cba
mygeone <32967596+mygeone@users.noreply.github.com>
2020-07-08 18:08:14 -0400

    08/07

--- ingresarOrden.php
+++ ingresarOrden.php
@@ -1,5 +1,5 @@
 <?php
-include("header.php");
+include_once("header.php");
 include("config.php");
 ?>
 
@@ -25,6 +25,7 @@ if($_GET['step'] == 0){
     }
 ?>
 
+
 <?php
 if($_GET['step'] == 1){
     $rutToVerify = pg_escape_string($_POST['rutCliente']);
@@ -36,7 +37,6 @@ if($_GET['step'] == 1){
         '
         <div class="container">
             <div class="row">
-
                 <!-- RUT DISABLED CLIENTE-->
                 <div class="col-8">
                     <div class="container mt-5">
@@ -48,63 +48,46 @@ if($_GET['step'] == 1){
                                             <label for="disabledTextInput">Ingrese Rut Cliente</label>
                                             <input type="text" id="disabledTextInput" class="form-control" placeholder="'.$_POST['rutCliente'].'">
                                         </div>
-                                    </fielset>
+                                    </fieldset>
                                 </form>
                             </div>
                         </div>
                     </div>
 
 
-                    <!-- SELECCION DE PRODUCTO-->
-                    
-                    <div class="container mt-3">
-                        <form method="POST" action="/ProyectoBDD/addToCarrito.php">
-                        <div class="row">
-                            <div class="col-5">
-                                <div class="form-group">
-                                    <label for="exampleFormControlSelect1">Seleccione un producto</label>
-                                    <select class="form-control" id="exampleFormControlSelect1">';
-                                        $q = pg_query($conexion, 'select * from public."Productos" ');
-                                        $row = pg_fetch_all($q);
-                                        print_r($row);
-                                        foreach($row as $index => $prod){
-                                        echo $prod['Nombre_Prod'];
-                                        echo "<option>".$prod['Nombre_Prod']."  $".$prod['Precio_Prod']."</option>";
-                                        }
-                                        echo '
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-2">
-                                <div class="form-group">
-                                    <label for="exampleFormControlInput1">Cantidad</label>
-                                    <input type="number" class="form-control" id="exampleFormControlInput1" name="can" placeholder="1">
-                                </div>
-                            </div>
-                            <div class="col-2 d-flex align-items-center mt-3"><a class="btn btn-primary" href="ingresarMetodoPago.php" role="button">Agregar</a></div>
-                            </div>
-                        </div>
-                        </form>
-                    <div class="row justify-content-center mt-5">
-                        <div class="col-3 d-flex align-items-center"><a class="btn btn-primary" href="ingresarMetodoPago.php" role="button">Ingresar Otro Producto</a></div>
-                        <div class="col-2 d-flex align-items-center"><a class="btn btn-primary" href="ingresarMetodoPago.php" role="button">Finalizar</a></div>
-                    </div>
-                </div>
+                    <select id="nameProducts">
+                        <option value="">Select Product</option>';
+                            $query = 'SELECT * FROM public."Productos" '; 
+                            $result = pg_query($conexion,$query );
+                            if(pg_num_rows($result) > 0){ 
+                                while($row = pg_fetch_assoc($result)){
+                                    echo '<option value="'.$row['Prod_ID'].'">'.$row['Nombre_Prod'].'</option>'; 
+                                }
+                            }else{ 
+                                echo '<option value="">Product not available</option>'; 
+                            };
+       echo '      </select>
 
-                <!-- CARRITO -->
-                <div class="col-4">CARRITO</div>
+
+                    <!-- Quantity dropdown -->
+                    <select id="stock">
+                        <option value="">Select Quantity</option>
+                    </select>
+                </div>
             </div>
         </div>
-        ';    
+            ';    
     }else{
         echo
-        '
-        <div class="displa-4">Rut no existe</div>
-        ';
-        header("Refresh: 3;url=/proyectoBDD/registerUser.php");
-        }
-}
-?>
+            '
+            <div class="display-4">Rut no existe</div>';
+            #header("Location: 3;url=/proyectoBDD/registerUser.php");
+        } ?>
+
+
+
+<?php } ?>
+
 
 
 </div>
